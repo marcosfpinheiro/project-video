0a1e89ad4cfac519b9ef8e605cbf57ec041682c4
marcosfpinheiro <pinheiro.tech@gmail.com>
2019-04-08 11:14:44 -0300

    feat: add google images search

---
+++ robots/image.js
@@ -0,0 +1,44 @@
+const google = require('googleapis').google
+const customSearch = google.customsearch('v1')
+const state = require('./state.js')
+
+const googleSearchCredentials = require('../credentials/google-search.json')
+
+
+async function robot(){
+	const content = state.load()
+
+  await fetchImagesOfAllSentences(content)
+	state.save(content)
+
+	async function fetchImagesOfAllSentences(content) {
+		for(const sentence of content.sentences){
+			const query = `${content.searchTerm} ${sentence.keywords[0]}`
+			sentence.images = await fetchGoogleAndReturnImagesLinks(query)
+
+			sentence.googleSearchQuery = query
+		}
+	}
+
+	async function fetchGoogleAndReturnImagesLinks(query){
+		const response = await customSearch.cse.list({
+			auth: googleSearchCredentials.apiKey,
+			cx: googleSearchCredentials.searchEngineId,
+			q: 'Michael Jackson',
+			searchType: 'image',
+			imgSize: 'huge',
+			num: 2
+	})
+
+		const imageURL = response.data.items.map((item) => {
+			return item.link	
+		})
+	
+		return imageURL
+}
+
+
+	
+}
+
+module.exports = robot
